321d51b5aa793a32b5d2c326c3b7a44b2f9ba4de
Dianauguerra <diana.1609@hotmail.com>
2026-03-05 22:59:07 -0700

    c

--- routes/static.js
+++ routes/static.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
