53d5690109e392dfa17e40a0eebc9ac74231228f
Dianauguerra <diana.1609@hotmail.com>
2026-03-14 16:44:37 -0600

    c

--- utilities/index.js
+++ utilities/index.js
@@ -1,8 +1,11 @@
+// utilities/index.js
 async function getNav() {
   return `
-  <ul>
-    <li><a href="/">Home</a></li>
-  </ul>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/inventory">Inventory</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
   `
 }
 
