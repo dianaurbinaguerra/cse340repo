e09f0abd70c9afaaa23a233ac76515487b2a8f54
Dianauguerra <diana.1609@hotmail.com>
2026-03-16 19:53:52 -0600

    render

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(async (req,res,next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
+  res.render("errors/error.js", {
     title: err.status || 'Server Error',
     message: err.message,
     nav
