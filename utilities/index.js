bc798411c3cc6e022c7171b1de7f29828135d644
Dianauguerra <diana.1609@hotmail.com>
2026-03-16 19:50:34 -0600

    utilities / index

--- utilities/index.js
+++ utilities/index.js
@@ -1,12 +1,27 @@
-// utilities/index.js
-async function getNav() {
-  return `
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/inventory">Inventory</a></li>
-      <li><a href="/about">About</a></li>
-    </ul>
-  `
+const invModel = require("../models/inventory-model")
+const Util = {}
+
+/* ************************
+ * Constructs the nav HTML unordered list
+ ************************** */
+Util.getNav = async function (req, res, next) {
+  let data = await invModel.getClassifications()
+  let list = "<ul>"
+  list += '<li><a href="/" title="Home page">Home</a></li>'
+  data.rows.forEach((row) => {
+    list += "<li>"
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>"
+    list += "</li>"
+  })
+  list += "</ul>"
+  return list
 }
 
-module.exports = { getNav }
+module.exports = Util
