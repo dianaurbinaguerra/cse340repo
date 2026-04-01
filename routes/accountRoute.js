78e81e43881d7076dbe83b3c3cb628625d5cb50b
Dianauguerra <diana.1609@hotmail.com>
2026-03-31 19:56:21 -0600

    c

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
 
 /* *******************************
  * Deliver Login View
@@ -15,4 +16,6 @@ const utilities = require("../utilities")
 
 router.get( "/login",utilities.handleErrors(accountController.buildLogin))
 
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
 module.exports = router 
