819f63986338eaa1c66d62204188db8e4f7cff7e
Dianauguerra <diana.1609@hotmail.com>
2026-04-02 18:57:30 -0600

    commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,12 +7,15 @@ baseController.buildHome = async function(req, res){
   res.render("index", {title: "Home", nav})
 }
 
-/* *********************************
- * Task 3 Trigger a 500 Server Error
- * ****************************** */
-baseController.triggerError = async function (req, res, next) {
-  throw new Error("500 Server Error")  
+/* ***************************
+ *  Build Home view with MVC
+ *  Unit 3, MVC: Get Started Activity
+ *  Flash message Unit 4, Sessions & Messages activity
+ * ************************** */
+baseController.buildHome = async function (req, res) {
+  const nav = await utilities.getNav()
+  // req.flash("notice", "This is a flash message.")
+  res.render("index", { title: "Home", nav })
 }
-
 
 module.exports = baseController
